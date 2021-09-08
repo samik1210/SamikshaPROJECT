3b66a6968c4c3e6a7ad38aeda51549bfbec1a6c8
samik1210 <65801542+samik1210@users.noreply.github.com>
2021-09-08 17:46:52 +0530

    Add files via upload

---
+++ Studentproject.sql
@@ -0,0 +1,13 @@
+use student;
+Create table stu1
+(
+Stud_ID int primary key,
+Stud_FirstName varchar(32),
+Stud_Lastname varchar(32),
+Stud_Address varchar(50),
+Stud_Age int,
+Stud_Email varchar(32) not null,
+Stud_Trainer varchar(32),
+Stud_Batch varchar(32)
+ 
+)  
